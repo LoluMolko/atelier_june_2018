b177644dc8224d5b270509744dff8b096c1e503d
Marta Kosmala <marta.kos1230@gmail.com>
2018-06-20 21:13:15 +0200

    Add API for books search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,30 @@ class BooksController < ApplicationController
   def destroy
   end
 
+  def filter
+    render template: 'books/filter', locals: { books: filter_books }
+  end
+
   private
+
+  def filter_params
+    permitted_params
+      .slice(:title, :isbn)
+      .merge(category.present? ? { category_id: category.id } : {})
+      .reject{ |k, v| v.to_s.empty? }
+  end
+
+  def filter_books
+    Book.where(filter_params)
+  end
+
+  def category
+    Category.find_by(name: permitted_params[:category_name])
+  end
+
+  def permitted_params
+    params.permit(:title, :isbn, :category_id, :category_name)
+  end
 
   def load_books
     @books = Book.all
